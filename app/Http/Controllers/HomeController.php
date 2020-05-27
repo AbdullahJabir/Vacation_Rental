3696f9ec8369d9776460c8e11e2d2e9bd94601b2
Abdulla1Jabir <jabiralgazi94@gmail.com>
2020-05-27 11:49:28 +0600

    apartmentadd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\AjaxCrud;
 use App\vacationleft;
-
+use App\ApartmentRoom;
 class HomeController extends Controller
 {
     public function index(){
     	 $items = AjaxCrud::all();
     	 $vacation_rental=vacationleft::all();
-       return view('welcome',compact('items','vacation_rental'));
+    	 $ApartmentRoom = ApartmentRoom::all();
+       return view('welcome',compact('items','vacation_rental','ApartmentRoom'));
 
     }
 }
